8365814c992b95153ec3ed411c0d91499eca6566
mateus.franco <mateus.franco@sicoob.com.br>
2024-07-30 14:42:15 -0300

    Creating Spring Boot Project

---
+++ src/main/java/authorization/transactionauthorizer/TransactionAuthorizerApplication.java
@@ -0,0 +1,13 @@
+package authorization.transactionauthorizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionAuthorizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionAuthorizerApplication.class, args);
+    }
+
+}
